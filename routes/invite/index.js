2efc64330e6647ef021aa71681b4a2a0d05c52b7
Anand Murugan <anand.murugan@gmail.com>
2024-12-29 08:38:04 +0530

    refactor

--- routes/invite/index.js
+++ routes/invite/index.js
@@ -1,10 +1,10 @@
 'use strict'
 
 module.exports = async function (fastify, opts) {
-  fastify.get('/health', async function (request, reply) {
+  fastify.get('/', async function (request, reply) {
     return 'App is running.'
   });
-  fastify.post('/invite', async function (request, reply) {
+  fastify.post('/', async function (request, reply) {
     console.log('request.body:', request.body);
   })
 }

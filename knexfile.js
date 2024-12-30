5d8e8360f5fb9133f77fafe88c4777e2bed76e27
Anand Murugan <anand.murugan@gmail.com>
2024-12-30 22:36:55 +0530

    knexfile

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+require('dotenv').config(); // If using a .env file
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      ssl: { rejectUnauthorized: false },
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+  },
+};

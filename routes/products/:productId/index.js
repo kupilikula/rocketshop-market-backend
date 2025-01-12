75412e8ed573bf8af5cf75ac0c19f12954db86d0
Anand Murugan <anand.murugan@gmail.com>
2025-01-12 16:01:02 +0530

    fix bug

--- routes/products/:productId/index.js
+++ routes/products/:productId/index.js
@@ -11,10 +11,7 @@ module.exports = async function (fastify, opts) {
         return reply.status(404).send({ error: 'Product not found.' });
       }
 
-      const mediaItems = await knex('mediaItems')
-          .whereIn('mediaId', product.mediaItemIds || []);
-
-      return reply.send({ product, mediaItems });
+      return reply.send({ product });
     } catch (err) {
       request.log.error(err);
       return reply.status(500).send({ error: 'Failed to fetch product details.' });

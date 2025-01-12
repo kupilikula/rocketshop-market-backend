9760f133112589cdb82c11b393811fdc832cdf6b
Anand Murugan <anand.murugan@gmail.com>
2025-01-12 15:58:04 +0530

    fix bug

--- routes/products/:productId/index.js
+++ routes/products/:productId/index.js
@@ -1,11 +1,10 @@
 'use strict'
 
-const knex = require("knex");
+const knex = require("@database/knexInstance");
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async (request, reply) => {
     const { productId } = request.params;
-
     try {
       const product = await knex('products').where('productId', productId).first();
       if (!product) {

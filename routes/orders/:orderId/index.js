b2ad60466a3a5e23d84aa53712b3d47b298ca575
Anand Murugan <anand.murugan@gmail.com>
2025-01-18 12:20:30 +0530

    single order endpoint

--- routes/orders/:orderId/index.js
+++ routes/orders/:orderId/index.js
@@ -3,7 +3,7 @@
 const knex = require("@database/knexInstance");
 
 module.exports = async function (fastify, opts) {
-    fastify.get('/orders/:orderId', async (request, reply) => {
+    fastify.get('/', async (request, reply) => {
         const { orderId } = request.params;
 
         try {

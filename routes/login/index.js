dfdc6d3a73140047669f8aa37591eb7f59d65588
Anand Murugan <anand.murugan@gmail.com>
2025-01-26 12:14:46 +0530

    jwts

--- routes/login/index.js
+++ routes/login/index.js
@@ -1,10 +1,21 @@
 'use strict'
 
 const knex = require("@database/knexInstance");
+const {generateJWT} = require("../../utils/jwt");
 
 module.exports = async function (fastify, opts) {
     fastify.post('/', async function (request, reply) {
-        let i = request.body.customerIndex || 0;
+
+        const {phone, otp, customerIndex} = request.body;
+
+        // Validate user credentials here (e.g., check in database)
+        const isValid = true; // Replace with actual validation logic
+
+        if (!isValid) {
+            return reply.status(401).send({ error: 'Invalid credentials' });
+        }
+
+        let i = customerIndex || 0;
         let customer = await knex('customers')
             .offset(i)
             .first(); // Get a single row directly
@@ -13,6 +24,12 @@ module.exports = async function (fastify, opts) {
             throw new Error('No row found at the specified index');
         }
 
-        reply.status(200).send(customer);
+        // Create payload (e.g., customerId or merchantId)
+        const payload = { customerId: customer.customerId };
+
+        // Generate JWT
+        const token = generateJWT(payload);
+
+        reply.status(200).send({token, customer});
     });
 }

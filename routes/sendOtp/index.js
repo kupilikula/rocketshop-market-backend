168e5793095d9229af09de8e39a433d873023c40
Anand Murugan <anand.murugan@gmail.com>
2025-04-20 16:50:24 +0530

    disable sms

--- routes/sendOtp/index.js
+++ routes/sendOtp/index.js
@@ -51,17 +51,17 @@ module.exports = async function (fastify, opts) {
         console.log(`Sending OTP ${otp} to phone ${phone}`);
 
         // Generate OTP message and send SMS
-        try {
-            const message = getOtpText(otp);
-            await smsService.sendSMS(request.body.phone, message);
-        } catch (error) {
-            console.error('Failed to send OTP SMS:', error);
-            // Optionally, you might want to delete the OTP record if SMS fails
-            await knex('otp_verification')
-                .where({ phone: request.body.phone, otp })
-                .delete();
-            return reply.status(500).send({ error: 'Failed to send OTP' });
-        }
+        // try {
+        //     const message = getOtpText(otp);
+        //     await smsService.sendSMS(request.body.phone, message);
+        // } catch (error) {
+        //     console.error('Failed to send OTP SMS:', error);
+        //     // Optionally, you might want to delete the OTP record if SMS fails
+        //     await knex('otp_verification')
+        //         .where({ phone: request.body.phone, otp })
+        //         .delete();
+        //     return reply.status(500).send({ error: 'Failed to send OTP' });
+        // }
 
 
         if (context==='AUTH_LOGIN') {

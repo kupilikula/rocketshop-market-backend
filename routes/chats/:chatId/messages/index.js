fa64c44b233610105983851c8302c927144f0a3e
Anand Murugan <anand.murugan@gmail.com>
2025-04-22 15:31:13 +0530

    refactor chats

--- routes/chats/:chatId/messages/index.js
+++ routes/chats/:chatId/messages/index.js
@@ -3,7 +3,6 @@ const knex = require("@database/knexInstance");
 module.exports = async function (fastify, opts) {
     fastify.get("/", async (request, reply) => {
         const { chatId } = request.params;
-
         const customerId = request.user.customerId;
 
         if (!customerId) {
@@ -14,8 +13,7 @@ module.exports = async function (fastify, opts) {
             const messages = await knex("messages as m")
                 .leftJoin("message_reads as r", function () {
                     this.on("m.messageId", "=", "r.messageId")
-                        .andOn("r.readerId", "=", knex.raw("?", [customerId]))
-                        .andOn("r.readerType", "=", knex.raw("?", ["Customer"]));
+                        .andOn("r.readerType", "=", knex.raw("?", ["Merchant"])); // any merchant who read it
                 })
                 .select(
                     "m.messageId",
@@ -25,9 +23,10 @@ module.exports = async function (fastify, opts) {
                     "m.message",
                     "m.created_at",
                     "m.updated_at",
-                    "r.read_at"
+                    knex.raw("MAX(r.read_at) as read_at") // aggregated read timestamp
                 )
                 .where("m.chatId", chatId)
+                .groupBy("m.messageId", "m.chatId", "m.senderId", "m.senderType", "m.message", "m.created_at", "m.updated_at")
                 .orderBy("m.created_at", "asc");
 
             return reply.send(messages);

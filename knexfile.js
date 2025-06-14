0b133ed5591b50b75baf3931258acd3631381c38
Anand Murugan <anand.murugan@gmail.com>
2025-06-15 00:30:21 +0530

    prod deploy

--- knexfile.js
+++ knexfile.js
@@ -11,8 +11,16 @@ module.exports = {
       port: process.env.DB_PORT,
       ssl: { rejectUnauthorized: false },
     },
-    migrations: {
-      directory: './database/migrations',
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      ssl: { rejectUnauthorized: false },
     },
   },
 };
